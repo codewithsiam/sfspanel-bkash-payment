e5949cb54e266f87e54ee433b26b3b7cc13f1470
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-11-30 18:44:20 +0600

    button test

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import axios from 'axios'
+import axios from "axios";
 
 const Home = () => {
-
-    const pay = async () => {
-        try {
-            const { data } = await axios.post('http://localhost:5000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
-            window.location.href = data.bkashURL
-        } catch (error) {
-            console.log(error.response.data)
-        }
+  const pay = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/bkash/payment/create",
+        { amount: 50, orderId: 1 },
+        { withCredentials: true }
+      );
+      window.location.href = data.bkashURL;
+    } catch (error) {
+      console.log(error.response.data);
     }
-    return (
-        <div>
-            <button onClick={pay}>Pay bkash</button>
-        </div>
-    )
-}
+  };
+  return (
+    <div>
+      <button onClick={pay}>Pay bkash</button>
+      <button>Go to home</button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
